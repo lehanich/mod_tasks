99a53099e8fcbd62a0b8d3e3a55e2cfc25ad17ed
a.yakimkin <ayakimkin@gmail.com>
2022-11-21 19:00:39 +0300

    PriorityQ2: add priority based on LinkedList

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import addTask from "./addtask";
 import {Strategy} from "./queue/strategy";
 import StandartQ from "./queue/standart-q";
 import PriorityQ from "./queue/priority-q";
+import PriorityQ2 from "./queue/priority-q-2";
 import Task from "./tasks/task";
 import TaskIter from "./tasks/task-iter";
 import TaskPromise from "./tasks/task-promise";
@@ -11,6 +12,7 @@ export {
   Strategy,
   StandartQ,
   PriorityQ,
+  PriorityQ2,
   TaskIter,
   TaskPromise,
   Task,
---
+++ src/queue/priority-q-2.ts
@@ -0,0 +1,98 @@
+import { Priority,
+  StrategyAdapter,
+  WorkerContainer } from "../interface";
+import Queue from "./queue";
+import {ListNodeLink} from "./queue/LinkedList/interface";
+
+export default class PriorityQ2<T> implements StrategyAdapter {
+  #maxSize: number = 100;
+  #queue: Queue<WorkerContainer> = new Queue(100);
+  #length: number = 0;
+
+  constructor(size: number) {
+    this.#maxSize = size;
+    this.#queue = new Queue(size);
+    this.#length = 0;
+  }
+
+  insert(value: any, priority: Priority): void {
+    let j: number;
+    let item: WorkerContainer = [priority, value];
+
+    if (this.#length === 0) {
+      this.#queue.push(item);
+    } else {
+      switch(priority) {
+        case "low":
+          this.#queue.push(item)
+          break;
+
+        case "normal":
+          this.#queue.inserMiddle(item);
+          break;
+
+        case "height":
+          this.#queue.insertFirst(item);
+          break;
+        default: 
+
+      }
+    }
+
+    this.#length++;
+  }
+
+  remove(delWorker: any): void {
+    let findElem = this.#queue.findContainer(delWorker);
+    // if (findElem) {
+    this.#queue.remove(findElem);
+    // }
+    this.#length--;
+  }
+
+  peek(i: number): WorkerContainer | undefined {
+    let j = 0;
+    let current: ListNodeLink<WorkerContainer> = this.#queue.first;
+
+    while (i !== j) {
+      current = current?.next;
+      j++
+    };
+
+    return <WorkerContainer>current!.value
+  }
+
+  isEmpty() {
+    return this.#length === 0;
+  }
+
+  isFull() {
+    return this.#length === this.#maxSize;
+  }
+
+  pop(): WorkerContainer | undefined{
+    let item: WorkerContainer;
+
+    if (this.#length === 0) {
+      return undefined;
+    }
+
+    this.#length--;
+    item = this.#queue.pop()
+
+    return item;
+  }
+
+  push(item: WorkerContainer) {
+    this.#queue.push(item)
+    this.#length++;
+  }
+
+  get length() {
+    return this.#length;
+  }
+
+  get queue() {
+    return this.#queue;
+  }
+}
